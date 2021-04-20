4a6b3dc1aaf9be5aeec0ece804966ce09b016597
force-coder <8397513@qq.com>
2021-04-20 09:38:10 +0800

    feat: 初始化项目结构

---
+++ permission-server-dao/src/main/java/cn/force/coder/permission/entity/BaseEntity.java
@@ -0,0 +1,11 @@
+package cn.force.coder.permission.entity;
+
+import java.time.LocalDateTime;
+
+public class BaseEntity {
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+}
